a3a0a64a653044a8a41d4f3a4a8191cb509dd9f3
singhariaprinkle <31589320+singhariaprinkle@users.noreply.github.com>
2017-09-14 00:29:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,13 @@ img.onclick=function ()
 {
     img.style.marginLeft='100px';
 }
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick= function()
+{
+    var interval = setInterval(moveright,100);
+}
