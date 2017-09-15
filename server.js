d84868ef5faaf1f57d3b299accbe6a283fa9fee4
singhariaprinkle <31589320+singhariaprinkle@users.noreply.github.com>
2017-09-15 21:36:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counte=0;
 app.get('/counter',function(req, res)
 {
     counter= counter + 1;
