b4ed0aaa95e772ee4268ee35ffd0f4fcaa636752
singhariaprinkle <31589320+singhariaprinkle@users.noreply.github.com>
2017-09-15 21:30:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/counter',function(req, res)
+{
+    counter= counter + 1;
+    res.send(counter.tostring());
+});
 app.get('/article-one',function (req, res)
 {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
