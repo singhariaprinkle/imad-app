1fc57e8405c0f593ac8fb53e9c77e85959071361
singhariaprinkle <31589320+singhariaprinkle@users.noreply.github.com>
2017-09-16 18:23:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,25 @@
 //Counter code
 var button = document.getElementById('counter');
-button.onclick = function () {
+button.onclick = function () 
+{
   
-  //make a request to counter endpoint
-  var request = new XMLHttpRequest();
-  //capture the response and store it in a variable
-  request.onreadystatechange =function()
-  {
-      if(request.readyState===XMLHttpRequest.DONE)
+      //make a request to counter endpoint
+      var request = new XMLHttpRequest();
+      //capture the response and store it in a variable
+      request.onreadystatechange =function()
       {
-          if(request.status===200)
+          if(request.readyState===XMLHttpRequest.DONE)
           {
-              var counter=request.responseText;
-              var span =document.getElementById('count');
-              span.innerHTML = counter.toString();
+              if(request.status===200)
+              {
+                  var counter=request.responseText;
+                  var span =document.getElementById('count');
+                  span.innerHTML = counter.toString();
+              }
           }
-      }
-  };
-  //make request
-  request.open('GET','http://prinklesingharia.imad.hasura-app.io/counter',true);
-  request.send(null);
-  
+      };
+      //make request
+      request.open('GET','http://prinklesingharia.imad.hasura-app.io/counter',true);
+      request.send(null);
+      
 };
