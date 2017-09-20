97cdec2c5bb0b5685971e26d63105b679ea741b5
singhariaprinkle <31589320+singhariaprinkle@users.noreply.github.com>
2017-09-21 02:53:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool= new Pool(config);
 app.get('/test-db',function(req, res)
 {
    //make select request
